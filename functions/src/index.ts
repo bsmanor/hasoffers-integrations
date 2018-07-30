76ffc38afc99e732967ebc099f1a9c29e9ef625b
bsmanor <bsmanor@gmail.com>
2018-07-30 13:27:20 +0300

    update

---
+++ functions/src/index.ts
@@ -0,0 +1,9 @@
+import * as functions from 'firebase-functions';
+import axios from 'axios';
+const cors = require('cors')({origin: true});
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
